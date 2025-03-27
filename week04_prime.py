c009ccc935e765552017facf979513b4d6f2ac18
void <2021038@daelim.ac.kr>
2025-03-27 12:13:58 +0900

    v0.6 prime number

---
+++ week04_prime.py
@@ -0,0 +1,22 @@
+def is_prime(number):
+    """
+    Function to determine whether a number is a prime number
+    :param number: The number to determine whether it is prime or not
+    :return: Returns True if it is a prime number, False if it is not a prime number.
+    """
+    if number >= 2:
+        i = 2
+        while i * i <= number:
+            if number % i == 0:
+                return False
+            # print(i, end=" ")
+            i = i + 1
+    else:
+        return False
+    return True
+
+n = int(input())
+if is_prime(n):
+    print(f"{n}는(은) 소수입니다.")
+else:
+    print(f"{n}는(은) 소수가 아닙니다!")
