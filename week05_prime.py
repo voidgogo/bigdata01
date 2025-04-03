4fe549e6bfe45d5dbfca0780dfb9fa32fcca161f
void <2021038@daelim.ac.kr>
2025-04-03 12:26:19 +0900

    v1.0 prime number

--- week05_prime.py
+++ week05_prime.py
@@ -17,6 +17,9 @@ def is_prime(number : int) -> bool:
 n1 = int(input())
 n2 = int(input())
 
+if n1 > n2:
+    n1, n2 = n2, n1
+
 for i in range(n1, n2+1):
     if is_prime(i):
         print(i, end=' ')
