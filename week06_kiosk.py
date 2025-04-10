aebf449675a6656ba3d28bd614382174044b4611
void <2021038@daelim.ac.kr>
2025-04-10 12:00:44 +0900

    v1.6 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -33,9 +33,9 @@ while True:
     else:
         print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
 
-print("상품명 단가 수량 금액")
+print(f"{'상품명':^20}{'단가':^6}{'수량':^6}{'금액':^6}")
 for i in range(len(drinks)):
     if amounts[i] > 0:
-        print(f"{drinks[i]} {prices[i]} {amounts[i]} {prices[i] * amounts[i]}")
+        print(f"{drinks[i]:^20}{prices[i]:^6}{amounts[i]:^6}{prices[i] * amounts[i]:^6}")
 
 print(f"총 주문 금액 : {total_price}원")
