d0845a049b15e3f96747f502cf244786c8cdfdc0
void <2021038@daelim.ac.kr>
2025-04-10 10:34:39 +0900

    v0.4 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,11 +1,12 @@
 # 1) 아아 : 2000 2) 라떼 : 2500
+drinks = ["아이스 아메리카노", "카페 라떼"]
 prices = [1500, 2500]
 while True:
-    menu = input(f"1) 아이스 아메리카노 {prices[0]}원  2) 카페 라떼 {prices[1]}원  3) 주문종료 : ")
+    menu = input(f"1) {drinks[0]} {prices[0]}원  2) {drinks[1]} {prices[1]}원  3) 주문종료 : ")
     if menu == "1":
-        print(f"아이스 아메리카노를 주문하셨습니다. 가격은 {prices[0]}원 입니다")
+        print(f"{drinks[0]}를 주문하셨습니다. 가격은 {prices[0]}원 입니다")
     elif menu == "2":
-        print(f"카페 라떼를 주문하셨습니다. 가격은 {prices[1]}원 입니다")
+        print(f"{drinks[1]}를 주문하셨습니다. 가격은 {prices[1]}원 입니다")
     elif menu == "3":
         print("주문을 종료합니다")
         break
