8a82275a7916a7821831c117b7edf8ecc1c0aded
void <2021038@daelim.ac.kr>
2025-04-10 11:03:03 +0900

    v0.9 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -2,10 +2,19 @@
 drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스"]
 prices = [1500, 2500, 4000]
 amounts = [0, 0, 0]
+# drinks = ["아이스 아메리카노"]
+# prices = [1500]
+# amounts = [0]
 total_price = 0
 
+menu_texts = ""
+for j in range(len(drinks)):
+    menu_texts = menu_texts + f"{j+1}) {drinks[j]} {prices[j]}원  "
+menu_texts = menu_texts + f"{len(drinks)+1}) 주문종료 : "
+
 while True:
-    menu = input(f"1) {drinks[0]} {prices[0]}원  2) {drinks[1]} {prices[1]}원  3) {drinks[2]} {prices[2]}원  4) 주문종료 : ")
+   # menu = input(f"1) {drinks[0]} {prices[0]}원  2) {drinks[1]} {prices[1]}원  3) {drinks[2]} {prices[2]}원  4) 주문종료 : ")
+    menu = input(menu_texts)
     if menu == "1":
         print(f"{drinks[0]}를 주문하셨습니다. 가격은 {prices[0]}원 입니다")
         total_price = total_price + prices[0]
@@ -24,9 +33,6 @@ while True:
     else:
         print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
 
-# print(f"{drinks[0]} {prices[0]}원 {amounts[0]}잔 {prices[0] * amounts[0]}원")
-# print(f"{drinks[1]} {prices[1]}원 {amounts[1]}잔 {prices[1] * amounts[1]}원")
-# print(f"{drinks[2]} {prices[2]}원 {amounts[2]}잔 {prices[2] * amounts[2]}원")
 print("상품명 단가 수량 금액")
 for i in range(len(drinks)):
     print(f"{drinks[i]} {prices[i]} {amounts[i]} {prices[i] * amounts[i]}")
