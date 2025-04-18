f9dea9a4d73f44eebe14fa4ce10ceefbd3a75b79
void <2021038@daelim.ac.kr>
2025-04-18 11:24:22 +0900

    v2.5 kiosk

--- kiosk.py
+++ kiosk.py
@@ -41,3 +41,11 @@ def print_receipt() -> None:
             print(f"{drinks[i]:^20}{prices[i]:^6}{amounts[i]:^6}{prices[i] * amounts[i]:^6}")
 
     print(f"총 주문 금액 : {total_price}원")
+
+
+def test() -> None:
+    """
+    앞으로 추가될 키오스크 기능
+    :return:
+    """
+    pass
--- week09_kiosk.py
+++ week09_kiosk.py
@@ -1,4 +1,5 @@
-from kiosk import *
+# from kiosk import *
+from kiosk import drinks, display_menu, order_process, print_receipt
 
 while True:
     try:
@@ -14,3 +15,5 @@ while True:
         print(f"문자를 입력할 수 없습니다. 숫자를 입력해주세요")
 
 print_receipt()
+# test()
+# print(total_price)
