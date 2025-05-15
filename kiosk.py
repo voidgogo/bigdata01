afa5df4b439976e824c91641ab9af53cd32df16b
void <2021038@daelim.ac.kr>
2025-05-15 12:17:05 +0900

    v3.4 kiosk

--- kiosk.py
+++ kiosk.py
@@ -1,4 +1,5 @@
 import datetime
+import sqlite3
 
 drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스", "딸기 주스"]
 prices = [1500, 2500, 4000, 4200]
@@ -46,19 +47,27 @@ def print_ticket_number() -> None:
     주문 번호표 출력 함수
     :return: None
     """
-    try:
-        with open("ticket.txt", "r") as fp:
-            number = int(fp.read())
-    except FileNotFoundError:
-        number = 0
+    conn = sqlite3.connect('cafe.db')  # db instance open
+    cur = conn.cursor()
+    cur.execute('''
+        create table if not exists ticket (
+        id integer primary key autoincrement,
+        number integer not null
+        )
+    ''')
 
-    number = number + 1
+    cur.execute('select number from ticket order by number desc limit 1')
+    result = cur.fetchone()
 
-    with open("ticket.txt", "w") as fp:
-        fp.write(str(number))
+    if result is None:
+        number = 1
+        cur.execute('insert into ticket (number) values (?)', (number,))
+    else:
+        number = result[0] + 1
+        cur.execute('insert into ticket (number) values (?)', (number,))
 
+    conn.commit()
     print(f"번호표 : {number}")
-    # return number
 
 
 def order_process(idx: int) -> None:
@@ -104,6 +113,7 @@ def print_receipt() -> None:
     else:
         print(f"할인이 적용되지 않았습니다.\n지불하실 총 금액은 {total_price}원 입니다.")
     print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
 
 def test() -> None:
     """
