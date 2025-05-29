126d3e4c328ea1f34be298de0e332f3d088b20ae
void <2021038@daelim.ac.kr>
2025-05-29 10:38:39 +0900

    v3.9 kiosk

--- week13_kiosk.py
+++ week13_kiosk.py
@@ -3,9 +3,13 @@ import kiosk as kk
 import requests
 
 if __name__ == "__main__":
-    url = f"https://wttr.in/suwon?format=%C+%t&lang=ko"
+    # url = f"https://wttr.in/suwon?format=%C+%t&lang=ko"
+    url = f"https://naver.com/kim"  # 404 page not found
     response = requests.get(url)
-    print(response.text.strip())
+    if response.status_code == 200:
+        print(response.text.strip())
+    else:
+        print(f"상태 코드 : {response.status_code}")
     kk.run()
     kk.print_receipt()
     kk.print_ticket_number()
